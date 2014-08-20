d752007d2bdd66d0ec0abfe22123c933daaf478e
Henry Stephane (LudlowFx) <henry.steph@laposte.net>
2014-08-21 00:48:15 +0200

    Update 0.1.2f1

--- PlayMaker/Actions/SmartLocalization/ChangeLanguage.cs
+++ PlayMaker/Actions/SmartLocalization/ChangeLanguage.cs
@@ -1,15 +1,5 @@
 using UnityEngine;
 using SmartLocalization;
-using System.Collections.Generic;
-
-/*
- * BETA INFORMATIONS :
- * 
- * This bridge between PlayMaker and SmartLocalization is not an official package, but a personal version I try to adapt. So it is possible that many improvements are to be made.
- * 
- * This version is based on the Package of kdimas and extended to new shares by LudlowFx.
- * 
- */
 
 namespace HutongGames.PlayMaker.Actions
 {
--- PlayMaker/Actions/SmartLocalization/GetCodeLoadedLanguage.cs
+++ PlayMaker/Actions/SmartLocalization/GetCodeLoadedLanguage.cs
@@ -1,16 +1,6 @@
 using UnityEngine;
 using SmartLocalization;
 
-
-/*
- * BETA INFORMATIONS :
- * 
- * This bridge between PlayMaker and SmartLocalization is not an official package, but a personal version I try to adapt. So it is possible that many improvements are to be made.
- * 
- * This version is based on the Package of kdimas and extended to new shares by LudlowFx.
- * 
- */
-
 namespace HutongGames.PlayMaker.Actions
 {
     [ActionCategory("SmartLocalization (Beta)")]
